fea4005c42fcfbc4d980a23b0cde8a0be31e57c3
Kazefunet <13380661+kazefunet@user.noreply.gitee.com>
2025-03-08 21:30:14 +0800

    refactor(AntLLM):重构项目目录并优化代码
    - 重命名和移动了多个文件和目录，以更好地组织项目结构
    - 优化了部分代码实现，提高了可读性和可维护性
    - 更新了项目 README，以反映新的目录结构和使用方法- 删除了未使用的代码和文件，精简了项目

--- data/constant/model_provider.py
+++ data/constant/model_provider.py
@@ -1,4 +1,3 @@
-
 from langchain_ollama import OllamaLLM
 from langchain_ollama import ChatOllama
 base_url = "http://127.0.0.1:11434"
@@ -9,7 +8,6 @@ def initialize_llm(model:str):
     temperature=0.0,
     format="json"
     )
-
 def initialize_v_llm(model:str):
     return OllamaLLM(
     model=model,
